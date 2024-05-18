811793f73132d0f1d4df141efb2d767c17d20c31
Harsh Pandey <pandey73harsh@gmail.com>
2024-05-18 17:28:42 +0530

    add bg grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen  ">
+    <main className="min-h-screen relative ">
+      
         <div className="w-full p-2 flex justify-center items-center z-10 border-b border-[#a1a2a2]  ">
           <Navbar />
         </div>
-        <div className="w-full hero ">
+        <div className="w-full hero relative">
+        <div className="absolute bg-black w-full h-full -z-0 -top-0 left-0 bg-grid-white opacity-20 pointer-events-none inset-0 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
+      </div>
           <Hero />
         </div>
+        
+        <div className="min-h-screen"></div>
     </main>
   );
 }
